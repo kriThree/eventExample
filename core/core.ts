19ff586e286004d21733b09b50d159c4d0bb9177
kriThree <138926314+kriThree@users.noreply.github.com>
2023-11-13 22:21:26 +0300

    start

---
+++ core/core.ts
@@ -0,0 +1,11 @@
+class Model {
+	name: string;
+
+	constructor() {
+		this.name = "W";
+	}
+}
+
+const string = "eee";
+
+export { Model, string };
---
+++ server/src/index.ts
@@ -0,0 +1,30 @@
+import cors from "cors";
+import express, { json, urlencoded } from "express";
+import helmet from "helmet";
+
+
+const app = express();
+
+app.use(helmet());
+app.use(
+	cors({
+		origin: "*",
+	})
+);
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.get("*", (req, res) => {
+	console.log(req.path);
+
+	res.status(200).send("Success");
+});
+
+app.listen(4000, async () => {
+	try {
+		console.log("🚀 Server ready to handle requests");
+	} catch (e) {
+		console.log(e);
+	}
+});
